08ad3d82c2dd5b82e82b854e3fe9e951db29dcc7
Túlio Silva <84759095+tuliohfs@users.noreply.github.com>
2024-01-05 11:31:23 -0300

    Commit Adicionado a section Habilidades e sua estilização

--- static/script.js
+++ static/script.js
@@ -1,10 +1,22 @@
 /* abre e fecha menu lateral no mobile */
-const menuMobile = document.querySelector('.menu-mobile')
-const body = document.querySelector('body')
+const menuMobile = document.querySelector('.menu-mobile');
+const body = document.querySelector('body');
 
 menuMobile.addEventListener('click', () => {
     menuMobile.classList.contains("bi-list")
     ? menuMobile.classList.replace("bi-list", "bi-x")
-    : menuMobile.classList.replace("bi-x", "bi-list")
-    body.classList.toggle("menu-nav-active")
-})
+    : menuMobile.classList.replace("bi-x", "bi-list");
+    body.classList.toggle("menu-nav-active");
+});
+
+/* fecha o menu ao cliclar em uma sessão */
+const navItems = document.querySelectorAll('.nav-item');
+
+navItems.forEach(item => {
+    item.addEventListener("click", () => {
+        if(body.classList.contains("menu-nav-active")){
+            body.classList.remove("menu-nav-active");
+            menuMobile.classList.replace("bi-x", "bi-list");
+        }
+    });
+});
